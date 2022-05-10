50cdfe784730760a12836bae1ace426c961e1b5a
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2022-05-10 11:47:17 +1000

    css refactor for navheader

--- portfolio/components/Nav.js
+++ portfolio/components/Nav.js
@@ -5,7 +5,8 @@ import styles from "../styles/Home.module.css";
 const Nav = () => {
   return (
     <nav className={styles.navbar}>
-      <h1 className={styles.navheader}>kripukhadka</h1>
+      <h1 >kripukhadka</h1>
+      
     </nav>
   );
 }
