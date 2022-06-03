4a52d98926754ea17aab28c1fdf060c50c1efb67
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2022-06-03 18:33:05 +1000

    contentful integration

--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -8,6 +8,7 @@ import { mainImage, bodyImage } from '../images/blurData';
 
 
 
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -33,7 +34,6 @@ export default function Home() {
           >
             Hungry Jack&apos;s® Pty Ltd.
           </a>{" "}
-         
         </p>
         <div className={styles.grid}>
           <div className={styles.card}>
@@ -75,36 +75,10 @@ export default function Home() {
               alt="body image undraw"
             ></Image>
           </div>
-          {/* 
+
           
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a> */}
         </div>
+     
       </main>
       <Footer />
     </div>
